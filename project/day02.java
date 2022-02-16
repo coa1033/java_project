1094df80d8b011aad8611364a29c515426664c2b
건희 <99649524+coa1033@users.noreply.github.com>
2022-02-16 11:19:00 +0000

    project

---
+++ project/day02.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    
+    int age = 19;
+    System.out.println("제 나이는"+age+"살 입니다.");
+
+    if ( age < 20 ){
+      System.out.println("미성년 입니다.");
+    }
+  }
+}
